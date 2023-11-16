10dc1780add9985cc4ceaa2248e2af7874cc3aa7
Dexter <dexterbasergo@gmai.com>
2023-11-16 22:15:03 +0800

    MovieMOdal

--- src/MovieModal.js
+++ src/MovieModal.js
@@ -2,7 +2,6 @@ export default function MovieModal({ movie, setClicked }) {
   return (
     <div className="modal-container">
       <div className="modal">
-<<<<<<< HEAD
         <button className="close-button" onClick={() => setClicked(null)}>
           X
         </button>
@@ -15,11 +14,6 @@ export default function MovieModal({ movie, setClicked }) {
         <p className="plot">{movie.Plot}</p>
         <button className="mbttn"> Watch now</button>
 
-=======
-        <img src={movie.Poster} alt="poster" onClick={() => setClicked(null)}/>
-        <h1>{movie.Title}</h1>
-        <p>{movie.Plot}</p>
->>>>>>> 94885e73b0d13fb6284ebfbaef0e16c9bf07f148
       </div>
     </div>
   );
